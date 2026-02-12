d08b94f12a8eb8e9587433d871509093bbcd723a
ma-hub-be <maika.k1085@gmail.com>
2026-02-12 22:17:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -305,4 +305,6 @@ def compare_upload():
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
